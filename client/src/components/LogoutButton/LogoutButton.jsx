fbad95f478688354f70d48b0d53c86092a959b22
Koncke <martinkoncke@gmail.com>
2022-06-24 13:36:44 -0300

    Profile

--- client/src/components/LogoutButton/LogoutButton.jsx
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@mui/material/Button";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
@@ -9,9 +10,11 @@ const LogoutButton = () => {
       type="button"
       size="medium"
       variant="contained"
+      color="secondary"
+      startIcon={<LogoutIcon />}
       sx={{ mt: 3, mb: 2 }}
       onClick={() => logout({ returnTo: window.location.origin })}>
-      CERRAR SESION
+      LOGOUT
     </Button>
   );
 };
